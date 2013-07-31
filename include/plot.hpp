73b9a81ee06563a9687e35c6986c12c3b4189abc
cschreib <cschreib@orange.fr>
2013-07-31 22:43:28 +0200

    Implemented aligned formatted text.

--- include/plot.hpp
+++ include/plot.hpp
@@ -177,7 +177,7 @@ namespace plot {
                 }
             } else {
                 for (int_t i = 0; i < -level; ++i) {
-                    f -= pow(0.4*0.5, i+1)/0.5;
+                    f -= pow(0.4*0.5, i+1)/0.7;
                 }
             }
             return f;
@@ -650,11 +650,66 @@ namespace plot {
         }
 
         void show_line_(const std::string& text, float align) {
-            // TODO: move to formated string
-            define_("text", text);
-            file_ << "text stringwidth pop\n";
+            std::vector<fstring> fss;
+            parse_line_(text, fss);
+            int_t last_level = 0;
+            uint_t size = font_size_;
+            uint_t itxt = 0;
+
+            for (auto& fs : fss) {
+                if (fs.str.empty()) continue;
+
+                if (fs.level != last_level) {
+                    font(size*level_scale_(fs.level));
+                    last_level = fs.level;
+                }
+
+                ++itxt;
+                define_("text"+strn(itxt), fs.str);
+                file_ << "text"+strn(itxt)+" stringwidth pop";
+                if (itxt == 1) {
+                    file_ << "\n";
+                } else {
+                    file_ << " add\n";
+                }
+            }
+
+            if (last_level != 0) {
+                font(size);
+            }
+
             file_ << -clamp(align, 0, 1) << " mul 0 rmoveto\n";
-            file_ << "text show\n";
+
+            last_level = 0;
+            size = font_size_;
+            itxt = 0;
+            float height = char_height();
+            
+            for (auto& fs : fss) {
+                if (fs.str.empty()) continue;
+
+                if (fs.level != last_level) {
+                    float doffset = level_offset_(fs.level) - level_offset_(last_level);
+                    file_ << "0 " << height*doffset << " rmoveto\n";
+                    font(size*level_scale_(fs.level));
+                    last_level = fs.level;
+                }
+
+                ++itxt;
+                file_ << "text"+strn(itxt)+" show\n";
+            }
+
+            if (last_level != 0) {
+                float doffset = level_offset_(0) - level_offset_(last_level);
+                file_ << "0 " << height*doffset << " rmoveto\n";
+                font(size);
+            }
+
+            // TODO: move to formated string
+            // define_("text", text);
+            // file_ << "text stringwidth pop\n";
+            // file_ << -clamp(align, 0, 1) << " mul 0 rmoveto\n";
+            // file_ << "text show\n";
         }
 
         // Display some text at the current position (aligned : 0.0 = leftmost, 1.0 = rightmost)
