3bf3529923e11e2740ecaf995169d748f0913f52
= <cschreib@orange.fr>
2013-10-31 17:03:26 +0100

    Updated subregion().

--- include/image.hpp
+++ include/image.hpp
@@ -33,51 +33,51 @@ typename vec_t<2,Type>::effective_type enlarge(const vec_t<2,Type>& v, int_t pix
 
 // Get a sub region 'reg' inside an image. reg = {x0, y0, x1, y1}.
 template<typename TypeV, typename TypeR = int_t>
-void subregion(const vec_t<2,TypeV>& v, const vec_t<1,TypeR>& reg, vec1i& rr, vec1i& rs) {
+void subregion(const vec_t<2,TypeV>& v, const vec_t<1,TypeR>& reg, vec1u& rr, vec1u& rs) {
     assert(n_elements(reg) == 4);
     int_t nvx = v.dims[0], nvy = v.dims[1];
-    int_t nx = reg(2)-reg(0)+1, ny = reg(3)-reg(1)+1;
+    int_t nx = reg[2]-reg[0]+1, ny = reg[3]-reg[1]+1;
 
     vec1i vreg = reg;
     vec1i sreg = {0,0,nx-1,ny-1};
 
-    if (reg(0) >= nvx || reg(0) > reg(2) || reg(1) >= nvy || reg(1) > reg(3)) {
-        rr = rs = intarr(0);
+    if (reg[0] >= nvx || reg[0] > reg[2] || reg[1] >= nvy || reg[1] > reg[3]) {
+        rr = rs = uintarr(0);
         return;
     }
 
-    if (reg(0) < 0) {
-        vreg(0) = 0;
-        sreg(0) += 0 - reg(0);
+    if (reg[0] < 0) {
+        vreg[0] = 0;
+        sreg[0] += 0 - reg[0];
     }
-    if (reg(2) >= nvx) {
-        vreg(2) = nvx-1;
-        sreg(2) -= reg(2) - (nvx-1);
-    }
-
-    if (reg(1) < 0) {
-        vreg(1) = 0;
-        sreg(1) += 0 - reg(1);
-    }
-    if (reg(3) >= nvy) {
-        vreg(3) = nvy-1;
-        sreg(3) -= reg(3) - (nvy-1);
+    if (reg[2] >= nvx) {
+        vreg[2] = nvx-1;
+        sreg[2] -= reg[2] - (nvx-1);
     }
 
-    vec1i vx = rgen(vreg(0), vreg(2));
-    vec1i vy = rgen(vreg(1), vreg(3));
-    vec1i sx = rgen(sreg(0), sreg(2));
-    vec1i sy = rgen(sreg(1), sreg(3));
+    if (reg[1] < 0) {
+        vreg[1] = 0;
+        sreg[1] += 0 - reg[1];
+    }
+    if (reg[3] >= nvy) {
+        vreg[3] = nvy-1;
+        sreg[3] -= reg[3] - (nvy-1);
+    }
 
-    rr = flatten(indgen(nvx,nvy)(vx,vy));
-    rs = flatten(indgen(nx,ny)(sx,sy));
+    vec1u vx = rgen(vreg[0], vreg[2]);
+    vec1u vy = rgen(vreg[1], vreg[3]);
+    vec1u sx = rgen(sreg[0], sreg[2]);
+    vec1u sy = rgen(sreg[1], sreg[3]);
+
+    rr = flatten(uindgen(nvx,nvy)(vx,vy));
+    rs = flatten(uindgen(nx,ny)(sx,sy));
 }
 
 template<typename TypeV, typename TypeR>
 typename vec_t<2,TypeV>::effective_type subregion(const vec_t<2,TypeV>& v,
     const vec_t<1,TypeR>& reg, const typename vec_t<2,TypeV>::rtype& def = 0.0) {
 
-    vec1i rr, rs;
+    vec1u rr, rs;
     subregion(v, reg, rr, rs);
 
     int_t nx = reg(2)-reg(0)+1, ny = reg(3)-reg(1)+1;
