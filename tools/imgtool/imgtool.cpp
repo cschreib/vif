0e27b9a8c4a97f2d18bc003e2e466987d5b80e83
= <cschreib@orange.fr>
2014-05-19 16:12:47 +0200

    Added imgtool.

---
+++ tools/imgtool/imgtool.cpp
@@ -0,0 +1,105 @@
+#include <phypp.hpp>
+
+void print_help();
+
+bool convolve(int argc, char* argv[]);
+
+void print_convolve_help();
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        print_help();
+        return 0;
+    }
+
+    std::string op = tolower(argv[1]);
+
+    if (op == "help") {
+        op = tolower(argv[2]);
+        if (op == "convolve") {
+            print_convolve_help();
+        } else {
+            error("unknown operation '", op, "'");
+        }
+    } else {
+        if (op == "convolve") {
+            convolve(argc-1, argv+1);
+        } else {
+            error("unknown operation '", op, "'");
+        }
+    }
+
+    return 0;
+}
+
+void print_convolve_help() {
+    using namespace format;
+
+    paragraph("The program will convolve the first image (map) with the second image (kernel).");
+
+    header("List of available command line options:");
+    bullet("normalize", "[flag] normalize kernel to unit integral before convolution");
+    bullet("help", "[flag] print this text");
+    print("");
+}
+
+bool convolve(int argc, char* argv[]) {
+    if (argc < 4) {
+        error("usage: imgtool convolve [map.fits] [kernel.fits] [output.fits]");
+        return false;
+    }
+
+    bool help = false;
+    bool normalize = false;
+    read_args(argc-3, argv+3, arg_list(help, normalize));
+
+    if (help) {
+        print_convolve_help();
+        return true;
+    }
+
+    vec2d map = fits::read(argv[1]);
+    vec2d kernel = fits::read(argv[2]);
+    if (normalize) {
+        kernel /= total(kernel);
+    }
+
+    map = convolve2d(map, kernel);
+
+    file::mkdir(file::get_directory(argv[3]));
+    fits::write(argv[3], map);
+
+    return true;
+}
+
+void print_help() {
+    using namespace format;
+
+    print("imgtool v1.0");
+    header("Usage: imgtool operation [options]");
+    header("Available operations:");
+    bullet("convolve", "convolve a 2D kernel to a map");
+    print("");
+    header("To learn more about each operations and see the list of avilable options, run "
+        "'fitstool operation help'.");
+
+    print("");
+    paragraph("Copyright (c) 2014 C. Schreiber (corentin.schreiber@cea.fr)");
+
+    paragraph("This software is provided 'as-is', without any express or implied warranty. In no "
+        "event will the authors be held liable for any damages arising from the use of this "
+        "software.");
+
+    paragraph("Permission is granted to anyone to use this software for any purpose, including "
+        "commercial applications, and to alter it and redistribute it freely, subject to the "
+        "following restrictions:");
+
+    bullet("1", "The origin of this software must not be misrepresented; you must not claim that "
+        "you wrote the original software. If you use this software in a product, an acknowledgment "
+        "in the product documentation would be appreciated but is not required.");
+    bullet("2", "Altered source versions must be plainly marked as such, and must not be "
+        "misrepresented as being the original software.");
+    bullet("3", "This notice may not be removed or altered from any source distribution.");
+
+    print("");
+}
