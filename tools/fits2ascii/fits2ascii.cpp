ff211bfdcda067ab1bce22451323aca8db69bdcd
= <cschreib@orange.fr>
2015-01-13 12:52:34 +0100

    fits2ascii now supports filtering of rows

--- tools/fits2ascii/fits2ascii.cpp
+++ tools/fits2ascii/fits2ascii.cpp
@@ -1,22 +1,150 @@
 #include <phypp.hpp>
 
 template<std::size_t D, typename T>
-vec_t<D,std::string> read_column_(const std::string& filename, const std::string& colname, type_list<T>) {
-    vec_t<D,T> v;
-    fits::read_table(filename, colname, v);
-    return strna(v);
+void read_column_(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v, type_list<T>) {
+
+    vec_t<D,T> tmp;
+    fits::read_table(filename, colname, tmp);
+    v = strna(tmp);
 }
 
 template<std::size_t D>
-vec_t<D,std::string> read_column_(const std::string& filename, const std::string& colname, type_list<std::string>) {
-    vec_t<D,std::string> v;
+void read_column_(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v, type_list<std::string>) {
+
     fits::read_table(filename, colname, v);
-    return v;
+}
+
+enum class do_next {
+    run,
+    skip,
+    abort
+};
+
+do_next check_rows(const std::string& colname, uint_t tmp_row, uint_t& nrow,
+    const vec1u& ids) {
+
+    bool first_try = false;
+    if (nrow == 0) {
+        nrow = tmp_row;
+        first_try = true;
+    } else if (tmp_row != nrow) {
+        warning("incompatible number of rows for column '", colname, "' (got ",
+            tmp_row, ", expected ", nrow, ")");
+        note("this column will not be serialized");
+        return do_next::skip;
+    }
+
+    if (first_try && !ids.empty()) {
+        uint_t mid = max(ids);
+        if (mid >= nrow) {
+            error("maximum index in IDS is too large (got ", mid, ", against ",
+                nrow, ")");
+            return do_next::abort;
+        }
+    }
+
+    return do_next::run;
 }
 
 template<std::size_t D, typename T>
-vec_t<D,std::string> read_column(const std::string& filename, const std::string& colname) {
-    return read_column_<D>(filename, colname, type_list<T>{});
+do_next read_column_(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v, uint_t& nrow, const vec1u& ids, type_list<T>) {
+
+    vec_t<D,T> tmp;
+    fits::read_table(filename, colname, tmp);
+
+    auto next = check_rows(colname, tmp.dims[0], nrow, ids);
+    if (next != do_next::run) {
+        return next;
+    }
+
+    if (!ids.empty()) {
+        tmp = tmp(ids, repeat<D-1>(_));
+    }
+
+    v = strna(tmp);
+
+    return do_next::run;
+}
+
+template<std::size_t D>
+do_next read_column_(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v, uint_t& nrow, const vec1u& ids, type_list<std::string>) {
+
+    fits::read_table(filename, colname, v);
+
+    auto next = check_rows(colname, v.dims[0], nrow, ids);
+    if (next != do_next::run) {
+        return next;
+    }
+
+    if (!ids.empty()) {
+        v = v(ids, repeat<D-1>(_));
+    }
+
+    return do_next::run;
+}
+
+template<std::size_t D, typename T>
+void read_column(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v) {
+
+    read_column_<D>(filename, colname, v, type_list<T>{});
+}
+
+template<std::size_t D, typename T>
+do_next read_column(const std::string& filename, const std::string& colname,
+    vec_t<D,std::string>& v, uint_t& nrow, const vec1u& ids) {
+
+    return read_column_<D>(filename, colname, v, nrow, ids, type_list<T>{});
+}
+
+template<std::size_t Dim>
+do_next read_column(const std::string& in_file, const fits::column_info& cinfo,
+    vec_t<Dim,std::string>& v, std::string& vtype, uint_t& nrow, const vec1u& ids) {
+
+    switch (cinfo.type) {
+    case fits::column_info::string : {
+        auto next = read_column<Dim,std::string>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "string";
+        break;
+    }
+    case fits::column_info::boolean : {
+        auto next = read_column<Dim,bool>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "bool";
+        break;
+    }
+    case fits::column_info::byte : {
+        auto next = read_column<Dim,char>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "byte";
+        break;
+    }
+    case fits::column_info::integer : {
+        auto next = read_column<Dim,int_t>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "int";
+        break;
+    }
+    case fits::column_info::float_simple : {
+        auto next = read_column<Dim,float>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "float";
+        break;
+    }
+    case fits::column_info::float_double : {
+        auto next = read_column<Dim,double>(in_file, cinfo.name, v, nrow, ids);
+        if (next != do_next::run) return next;
+        vtype = "double";
+        break;
+    }
+    }
+
+    return do_next::run;
 }
 
 void print_help();
@@ -29,8 +157,11 @@ int main(int argc, char* argv[]) {
 
     vec1s tsplit;
     vec1s exclude, include;
+    std::string ids_file;
+    bool verbose = false;
 
-    read_args(argc-2, argv+2, arg_list(name(tsplit, "split"), exclude, include));
+    read_args(argc-2, argv+2, arg_list(name(tsplit, "split"), exclude, include,
+        name(ids_file, "ids"), verbose));
 
     std::string in_file = argv[1];
     std::string out_file = argv[2];
@@ -91,7 +222,8 @@ int main(int argc, char* argv[]) {
                     return 1;
                 }
 
-                vec1s st = read_column<1,std::string>(in_file, col);
+                vec1s st;
+                read_column<1,std::string>(in_file, col, st);
                 split_names[i] += tolower(st);
             } else {
                 split_names[i] += ss[j];
@@ -102,9 +234,16 @@ int main(int argc, char* argv[]) {
         split_names[i][ide] = split_cols[i] + '_' + strn(ide);
     }
 
+    vec1u id_sel;
+    if (!ids_file.empty()) {
+        fits::read_table(ids_file, "ids", id_sel);
+    }
+
     vec1s names;
     vec1s types;
     std::vector<vec1s> out_cols;
+    uint_t nrow_base = 0;
+
     for (auto& c : cols) {
         if (include.empty()) {
             if (!exclude.empty() && match_any_of(tolower(c.name), exclude)) continue;
@@ -112,36 +251,21 @@ int main(int argc, char* argv[]) {
             if (!match_any_of(tolower(c.name), include)) continue;
         }
 
+        if (verbose) {
+            print("found column: ", c.name);
+        }
+
         if (c.dims.size() == 1) {
             names.push_back(tolower(c.name));
-            std::string type;
-            switch (c.type) {
-            case fits::column_info::string :
-                out_cols.push_back(read_column<1,std::string>(in_file, c.name));
-                type = "string";
-                break;
-            case fits::column_info::boolean :
-                out_cols.push_back(read_column<1,bool>(in_file, c.name));
-                type = "bool";
-                break;
-            case fits::column_info::byte :
-                out_cols.push_back(read_column<1,char>(in_file, c.name));
-                type = "byte";
-                break;
-            case fits::column_info::integer :
-                out_cols.push_back(read_column<1,int_t>(in_file, c.name));
-                type = "int";
-                break;
-            case fits::column_info::float_simple :
-                out_cols.push_back(read_column<1,float>(in_file, c.name));
-                type = "float";
-                break;
-            case fits::column_info::float_double :
-                out_cols.push_back(read_column<1,double>(in_file, c.name));
-                type = "double";
-                break;
-            }
 
+            std::string type;
+            vec1s v;
+
+            auto next = read_column<1>(in_file, c, v, type, nrow_base, id_sel);
+            if (next == do_next::skip) continue;
+            if (next == do_next::abort) return 1;
+
+            out_cols.push_back(v);
             types.push_back("["+type+"]");
         } else if (c.dims.size() == 2) {
             vec1u i2c = where(split_cols == c.name);
@@ -151,45 +275,23 @@ int main(int argc, char* argv[]) {
                 continue;
             }
 
-            vec2s cols2;
             std::string type;
-            switch (c.type) {
-            case fits::column_info::string :
-                cols2 = read_column<2,std::string>(in_file, c.name);
-                type = "string";
-                break;
-            case fits::column_info::boolean :
-                cols2 = read_column<2,bool>(in_file, c.name);
-                type = "bool";
-                break;
-            case fits::column_info::byte :
-                cols2 = read_column<2,char>(in_file, c.name);
-                type = "byte";
-                break;
-            case fits::column_info::integer :
-                cols2 = read_column<2,int_t>(in_file, c.name);
-                type = "int";
-                break;
-            case fits::column_info::float_simple :
-                cols2 = read_column<2,float>(in_file, c.name);
-                type = "float";
-                break;
-            case fits::column_info::float_double :
-                cols2 = read_column<2,double>(in_file, c.name);
-                type = "double";
-                break;
-            }
+            vec2s v;
 
-            if (split_names[i2c[0]].size() != cols2.dims[1]) {
+            auto next = read_column<2>(in_file, c, v, type, nrow_base, id_sel);
+            if (next == do_next::skip) continue;
+            if (next == do_next::abort) return 1;
+
+            if (split_names[i2c[0]].size() != v.dims[1]) {
                 error("incompatible name list and column dimension (",
-                    split_names[i2c[0]].size(), " vs ", cols2.dims[1], ")");
+                    split_names[i2c[0]].size(), " vs ", v.dims[1], ")");
                 return 1;
             }
 
             append(names, split_names[i2c[0]]);
-            append(types, replicate("["+type+"]", cols2.dims[1]));
-            for (uint_t i : range(cols2.dims[1])) {
-                out_cols.push_back(cols2(_,i));
+            append(types, replicate("["+type+"]", v.dims[1]));
+            for (uint_t i : range(v.dims[1])) {
+                out_cols.push_back(v(_,i));
             }
         } else {
             warning(c.dims.size(), "-dimensional column '", c.name, "' is not supported");
@@ -197,20 +299,13 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    vec1b skip(out_cols.size());
-    uint_t nrow = 0;
+    uint_t nrow = nrow_base;
+    if (!id_sel.empty()) nrow = id_sel.size();
+
+    if (verbose) note("serializing ", out_cols.size(), " columns of ", nrow, " rows");
+
     vec1u colsize(out_cols.size());
     for (uint_t ic : range(out_cols.size())) {
-        if (nrow == 0) {
-            nrow = out_cols[ic].size();
-        } else if (out_cols[ic].size() != nrow) {
-            warning("incompatible number of rows for column '", names[ic], "' (got ",
-                out_cols[ic].size(), ", expected ", nrow, ")");
-            note("this column will not be serialized");
-            skip[ic] = true;
-            continue;
-        }
-
         colsize[ic] = max(vec1u{
             names[ic].length(), types[ic].length(), max(length(out_cols[ic]))
         });
@@ -218,23 +313,19 @@ int main(int argc, char* argv[]) {
 
     const uint_t padding = 2;
     for (uint_t ic : range(out_cols.size())) {
-        if (skip[ic]) continue;
         names[ic] = align_left(names[ic], colsize[ic]+padding);
         types[ic] = align_left(types[ic], colsize[ic]+padding);
         out_cols[ic] = align_left(out_cols[ic], colsize[ic]+padding);
     }
 
-    vec1u idi = where(!skip);
-
     std::ofstream out(out_file);
-    out << "# " << collapse(names[idi]) << "\n";
-    out << "# " << collapse(types[idi]) << "\n";
+    out << "# " << collapse(names) << "\n";
+    out << "# " << collapse(types) << "\n";
     out << "# \n";
 
     for (uint_t j : range(nrow)) {
         out << "  ";
         for (uint_t ic : range(out_cols.size())) {
-            if (skip[ic]) continue;
             out << out_cols[ic][j];
         }
         out << "\n";
