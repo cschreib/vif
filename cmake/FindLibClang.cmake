665eb7bcc3b2499d46beb7623d7cd863006b42b1
cschreib <cschreib@orange.fr>
2015-03-21 21:27:52 +0100

    Added libclang-3.5 to CMake scripts.

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -5,10 +5,12 @@ endif()
 find_path(CLANG_INCLUDE clang-c/Index.h
           HINTS
           ${CLANG_ROOT}/include
+          /usr/lib/llvm-3.5/include
           /usr/lib/llvm-3.4/include
           /usr/lib/llvm-3.3/include
           /usr/lib/llvm-3.2/include
           /usr/lib/llvm-3.1/include
+          /opt/local/libexec/llvm-3.5/include
           /opt/local/libexec/llvm-3.4/include
           /opt/local/libexec/llvm-3.3/include
           /opt/local/libexec/llvm-3.2/include
@@ -21,12 +23,15 @@ endif ()
 find_path(CLANG_COMPILATION_INCLUDE clang-c/CXCompilationDatabase.h
       HINTS
       ${CLANG_ROOT}/include
+      /opt/local/libexec/llvm-3.5/include
       /opt/local/libexec/llvm-3.4/include
       /opt/local/libexec/llvm-3.3/include
       /opt/local/libexec/llvm-3.2/include
       /opt/local/libexec/llvm-3.1/include
       /opt/local/libexec/llvm-3.0/include
+      /usr/lib/llvm-3.5/include
       /usr/lib/llvm-3.4/include
+      /usr/lib/llvm-3.3/include
       NO_DEFAULT_PATH)
 if (NOT EXISTS ${CLANG_COMPILATION_INCLUDE})
   find_path(CLANG_COMPILATION_INCLUDE clang-c/CXCompilationDatabase.h)
