2f4653ff9fd7722adca2202d622baec4a52fb0c8
= <cschreib@orange.fr>
2015-05-21 13:21:35 +0200

    subsrc can now work with ASCII input tables

--- tools/subsrc/subsrc.cpp
+++ tools/subsrc/subsrc.cpp
@@ -12,13 +12,13 @@ int main(int argc, char* argv[]) {
     std::string psf_model;
     vec1u include, exclude;
     std::string out, reg;
-    double map_unit = 1.0;
+    double fconv = 1.0;
     read_args(argc-2, argv+2, arg_list(
-        bands_notes, bands, notes, name(psf_model, "psf"), include, exclude, out, map_unit, reg
+        bands_notes, bands, notes, name(psf_model, "psf"), include, exclude, out, fconv, reg
     ));
 
     if (psf_model.empty()) {
-        error("missing PSF model");
+        error("missing PSF model: psf=...");
         return 1;
     }
 
@@ -27,94 +27,114 @@ int main(int argc, char* argv[]) {
     fits::read(argv[1], img, hdr);
 
     struct {
-        vec1u id;
         vec1d ra, dec;
-        vec2f flux;
-        vec1s bands, notes;
+        vec1f flux;
     } cat;
 
-    fits::read_table_loose(argv[2], cat);
-    if (cat.ra.empty()) {
-        fits::read_table_loose(argv[2], "pos.ra", cat.ra, "pos.dec", cat.dec);
-        if (cat.ra.empty()) {
-            error("could not find position in this catalog (either RA/DEC or POS.RA/POS.DEC)");
+    std::string cat_file = argv[2];
+    if (end_with(cat_file, ".fits")) {
+        struct {
+            vec1u id;
+            vec1d ra, dec;
+            vec2f flux;
+            vec1s bands, notes;
+        } fcat;
+
+        fits::read_table_loose(cat_file, fcat);
+        if (fcat.ra.empty()) {
+            fits::read_table_loose(cat_file, "pos.ra", fcat.ra, "pos.dec", fcat.dec);
+            if (fcat.ra.empty()) {
+                error("could not find position in this catalog (either RA/DEC or POS.RA/POS.DEC)");
+                return 1;
+            }
+        }
+
+        if (fcat.id.empty()) {
+            fcat.id = uindgen(fcat.ra.size());
+        }
+
+        vec1b bm, nm;
+        if (bands_notes.empty()) {
+            if (bands.empty() || fcat.bands.empty()) {
+                bm = replicate(true, fcat.flux.dims[1]);
+            } else {
+                bm = regex_match(fcat.bands, bands);
+            }
+            if (notes.empty() || fcat.notes.empty()) {
+                nm = replicate(true, fcat.flux.dims[1]);
+            } else {
+                nm = regex_match(fcat.notes, notes);
+            }
+        } else {
+            if (fcat.bands.empty()) {
+                bm = replicate(true, fcat.flux.dims[1]);
+            } else if (fcat.notes.empty()) {
+                bm = regex_match(fcat.bands, bands_notes);
+            } else {
+                bm = regex_match(fcat.bands+" "+fcat.notes, bands_notes);
+            }
+
+            nm = replicate(true, fcat.flux.dims[1]);
+        }
+
+        vec1u ib = where(nm && bm);
+        if (ib.empty()) {
+            error("could not find band");
+            return 1;
+        } else if (ib.size() != 1) {
+            error("multiple matches for band");
+            note(strn(fcat.bands[ib]+" "+fcat.notes[ib]));
             return 1;
         }
-    }
 
-    if (cat.id.empty()) {
-        cat.id = uindgen(cat.ra.size());
-    }
+        uint_t band = ib[0];
 
-    vec1b bm, nm;
-    if (bands_notes.empty()) {
-        if (bands.empty() || cat.bands.empty()) {
-            bm = replicate(true, cat.flux.dims[1]);
-        } else {
-            bm = regex_match(cat.bands, bands);
-        }
-        if (notes.empty() || cat.notes.empty()) {
-            nm = replicate(true, cat.flux.dims[1]);
-        } else {
-            nm = regex_match(cat.notes, notes);
-        }
+        vec1u id = where(
+            (include.empty() ? !is_any_of(fcat.id, exclude) : is_any_of(fcat.id, include)) &&
+            is_finite(fcat.flux(_,band)) && fcat.flux(_,band) > 0.0
+        );
+
+        cat.ra = fcat.ra[id];
+        cat.dec = fcat.dec[id];
+        cat.flux = fcat.flux(id,band);
     } else {
-        if (cat.bands.empty()) {
-            bm = replicate(true, cat.flux.dims[1]);
-        } else if (cat.notes.empty()) {
-            bm = regex_match(cat.bands, bands_notes);
-        } else {
-            bm = regex_match(cat.bands+" "+cat.notes, bands_notes);
-        }
-
-        nm = replicate(true, cat.flux.dims[1]);
+        vec1f flx;
+        file::read_table(cat_file, file::find_skip(cat_file), cat.ra, cat.dec, cat.flux);
     }
 
-    vec1u ib = where(nm && bm);
-    if (ib.empty()) {
-        error("could not find band");
-        return 1;
-    } else if (ib.size() != 1) {
-        error("multiple matches for band");
-        note(strn(cat.bands[ib]+" "+cat.notes[ib]));
-        return 1;
-    }
-
-    vec1u id = where(
-        (include.empty() ? !is_any_of(cat.id, exclude) : is_any_of(cat.id, include)) &&
-        is_finite(cat.flux(_,ib[0])) && cat.flux(_,ib[0]) > 0.0
-    );
-
-    print(id.size(), " sources to subtract from the image");
+    print(cat.ra.size(), " sources to subtract from the image");
 
     fits::wcs astro(hdr);
     vec1d x, y;
-    fits::ad2xy(astro, cat.ra[id], cat.dec[id], x, y);
+    fits::ad2xy(astro, cat.ra, cat.dec, x, y);
 
     vec2d psf(img.dims);
-    auto pg = progress_start(id.size());
-    for (uint_t i : range(id)) {
+    auto pg = progress_start(cat.ra.size());
+    for (uint_t i : range(cat.ra)) {
         if (!make_psf({{img.dims[0], img.dims[1]}}, y[i]-1, x[i]-1, psf_model, psf)) {
             return 1;
         }
 
-        img -= map_unit*psf*cat.flux(id[i],ib[0]);
+        img -= psf*cat.flux[i]/fconv;
         progress(pg);
     }
 
     if (out.empty()) {
-        out = erase_end(argv[1], 5)+"_res.fits";
+        out = file::remove_extension(argv[1])+"_res.fits";
     }
 
     fits::write(out, img, hdr);
 
     if (!reg.empty()) {
-        print_radec(reg, cat.ra[id], cat.dec[id]);
+        print_radec(reg, cat.ra, cat.dec);
     }
 
     return 0;
 }
 
 void print_help() {
-    print("[WIP]");
+    using namespace format;
+
+    print("subsrc v1.0");
+    paragraph("usage: subsrc img.fits srcs.[fits/cat] [psf=...,out=...]");
 }
