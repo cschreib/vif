5c4fc6bf4d0b0376d8daac2b17539dfc2b506a29
cschreib <cschreib@orange.fr>
2015-10-29 20:12:28 +0100

    Updated randsrc to new randpos & convex_hull API Generating clustered positions is no longer possible, and might be implemented back in a future update

--- tools/randsrc/randsrc.cpp
+++ tools/randsrc/randsrc.cpp
@@ -50,9 +50,9 @@ int main(int argc, char* argv[]) {
     }
 
     // Compute convex hull of input positions
-    vec1u hull = convex_hull(hra, hdec);
-    auto in_hull = [&](const vec1d& tra, const vec1d& tdec) {
-        return in_convex_hull(tra, tdec, hull, hra, hdec);
+    auto hull = build_convex_hull(hra, hdec);
+    auto in_hull = [&](double tra, double tdec) {
+        return in_convex_hull(tra, tdec, hull);
     };
 
     // Compute bounding box of input positions
@@ -66,50 +66,9 @@ int main(int argc, char* argv[]) {
     if (vmodel[0] == "uniform") {
         // Place points uniformly within the convex hull formed by the provided coordinates
         randpos_uniform_options opt;
-        opt.nsrc = nsrc;
         opt.max_iter = max_iter;
 
-        auto status = randpos_uniform(seed, rra, rdec, in_hull, ra, dec, opt);
-        if (!status.success) {
-            error("generation failed: ", status.failure);
-            return 1;
-        }
-    } else if (vmodel[0] == "power") {
-        // Place points with a power law angular correlation function within the convex
-        // hull formed by the provided coordinates, using the Soneira & Peebles algorithm.
-        // http://www.astro.rug.nl/~weygaert/tim1publication/lss2007/soneirapeebles.pdf
-
-        randpos_power_options opt;
-        opt.nsrc = nsrc;
-        opt.max_iter = max_iter;
-
-        auto iter = vmodel.begin()+1;
-
-        // Read the power law index in 'omega(theta) ~ r^(-power)'
-        if (!read_param(opt.power, 1.0, "power law index", iter, vmodel.end())) {
-            return 1;
-        }
-
-        // Read the number of levels to use in the generator
-        if (!read_param(opt.levels, 5, "number of levels", iter, vmodel.end())) {
-            return 1;
-        }
-
-        // Read the number of sources to generate and pick from
-        if (!read_param(opt.overload, 1.0, "overload factor", iter, vmodel.end())) {
-            return 1;
-        }
-
-        // Read the area inflation parameter
-        if (!read_param(opt.inflate, 1.0, "inflation factor", iter, vmodel.end())) {
-            return 1;
-        }
-
-        if (vmodel.size() > 5) {
-            warning("too many parameters provided (maximum is 4)");
-        }
-
-        auto status = randpos_power(seed, hull, hra, hdec, ra, dec, opt);
+        auto status = randpos_uniform_box(seed, nsrc, rra, rdec, ra, dec, in_hull, opt);
         if (!status.success) {
             error("generation failed: ", status.failure);
             return 1;
