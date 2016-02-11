b6573d5e79c78e6b53591ba241402ad31474a7e7
= <cschreib@orange.fr>
2016-02-11 11:34:41 +0100

    Added linfit_batch

--- include/phypp/math.hpp
+++ include/phypp/math.hpp
@@ -1913,6 +1913,90 @@ linfit_result linfit_pack(const vec<Dim,TypeY>& y, const vec<Dim,TypeE>& ye,
     return linfit_do_(y, ye, cache);
 }
 
+template<typename TypeE>
+struct linfit_batch_t {
+    const TypeE& ye;
+    vec2d cache;
+    vec1d beta;
+    vec2d alpha;
+    linfit_result fr;
+
+    template<typename ... Args>
+    linfit_batch_t(const TypeE& e, Args&&... args) : ye(e) {
+        uint_t np = sizeof...(Args);
+        uint_t nm = n_elements(ye);
+
+        cache.resize(np,nm);
+        beta.resize(np);
+        alpha.resize(np,np);
+        linfit_make_cache_(cache, ye, 0, std::forward<Args>(args)...);
+
+        // Solving 'y +/- e = sum over i of a[i]*x[i]' to get all a[i]'s
+        for (uint_t i = 0; i < np; ++i)
+        for (uint_t j = 0; j < np; ++j) {
+            if (i <= j) {
+                alpha.safe(i,j) = 0.0;
+                // alpha(i,j) = sum over all points of x[i]*x[j]/e^2
+                for (uint_t m = 0; m < nm; ++m) {
+                    alpha.safe(i,j) += cache.safe(i,m)*cache.safe(j,m);
+                }
+            } else {
+                alpha.safe(i,j) = alpha.safe(j,i);
+            }
+        }
+
+        if (!matrix::inplace_invert_symmetric(alpha)) {
+            fr.success = false;
+            fr.chi2 = dnan;
+            fr.params = replicate(dnan, np);
+            fr.errors = replicate(dnan, np);
+        } else {
+            fr.success = true;
+        }
+
+        matrix::symmetrize(alpha);
+        fr.cov = alpha;
+    }
+
+    template<typename TypeY>
+    void fit(const TypeY& y) {
+        phypp_check(same_dims_or_scalar(y, ye), "incompatible dimensions between Y and "
+            "YE arrays (", dim(y), " vs. ", dim(ye), ")");
+
+        if (!fr.success) return;
+
+        uint_t np = cache.dims[0];
+        uint_t nm = cache.dims[1];
+
+        auto tmp = flatten(y/ye);
+        for (uint_t i = 0; i < np; ++i) {
+            beta.safe[i] = 0.0;
+            // beta[i] = sum over all points of x[i]*y/e^2
+            for (uint_t m = 0; m < nm; ++m) {
+                beta.safe[i] += cache.safe(i,m)*tmp.safe[m];
+            }
+        }
+
+        fr.params = matrix::product(alpha, beta);
+        fr.errors = sqrt(matrix::diagonal(alpha));
+
+        vec1d model(nm);
+        for (uint_t m = 0; m < nm; ++m) {
+            model.safe[m] = 0.0;
+            for (uint_t i = 0; i < np; ++i) {
+                model.safe[m] += fr.params.safe[i]*cache.safe(i,m);
+            }
+        }
+
+        fr.chi2 = total(sqr(model - tmp));
+    }
+};
+
+template<typename TypeE, typename ... Args>
+linfit_batch_t<TypeE> linfit_batch(const TypeE& e, Args&&... args) {
+    return linfit_batch_t<TypeE>(e, std::forward<Args>(args)...);
+}
+
 struct affinefit_result {
     bool success;
 
