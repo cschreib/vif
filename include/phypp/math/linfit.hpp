52075810db1d75363b5409ab1a998d9e78719412
cschreib <cschreib@orange.fr>
2017-02-06 18:38:48 +0100

    Added linfit_pack_batch and modifying models in batches

--- include/phypp/math/linfit.hpp
+++ include/phypp/math/linfit.hpp
@@ -45,12 +45,12 @@ namespace phypp {
             vec2d alpha(np,np);
             vec1d beta(np);
             auto tmp = flatten(y/ye);
-            for (uint_t i = 0; i < np; ++i) {
-                for (uint_t j = 0; j < np; ++j) {
+            for (uint_t i : range(np)) {
+                for (uint_t j : range(np)) {
                     if (i <= j) {
                         alpha.safe(i,j) = 0.0;
                         // alpha(i,j) = sum over all points of x[i]*x[j]/e^2
-                        for (uint_t m = 0; m < nm; ++m) {
+                        for (uint_t m : range(nm)) {
                             alpha.safe(i,j) += cache.safe(i,m)*cache.safe(j,m);
                         }
                     } else {
@@ -60,7 +60,7 @@ namespace phypp {
 
                 beta.safe[i] = 0.0;
                 // beta[i] = sum over all points of x[i]*y/e^2
-                for (uint_t m = 0; m < nm; ++m) {
+                for (uint_t m : range(nm)) {
                     beta.safe[i] += cache.safe(i,m)*tmp.safe[m];
                 }
             }
@@ -82,9 +82,9 @@ namespace phypp {
             fr.cov = alpha;
 
             vec1d model(nm);
-            for (uint_t m = 0; m < nm; ++m) {
+            for (uint_t m : range(nm)) {
                 model.safe[m] = 0.0;
-                for (uint_t i = 0; i < np; ++i) {
+                for (uint_t i : range(np)) {
                     model.safe[m] += fr.params.safe[i]*cache.safe(i,m);
                 }
             }
@@ -143,10 +143,9 @@ namespace phypp {
         uint_t nm = y.size();
 
         vec2d cache(np,nm);
-        for (uint_t i = 0; i < np; ++i) {
-            for (uint_t j = 0; j < nm; ++j) {
-                cache.safe(i,j) = x.safe[i*x.pitch(0) + j]/ye.safe[j];
-            }
+        for (uint_t i : range(np))
+        for (uint_t j : range(nm)) {
+            cache.safe(i,j) = x.safe[i*x.pitch(0) + j]/ye.safe[j];
         }
 
         return impl::linfit_do_(y, ye, cache);
@@ -160,42 +159,117 @@ namespace phypp {
         vec2d alpha;
         linfit_result fr;
 
-        template<typename ... Args>
-        linfit_batch_t(const TypeE& e, Args&&... args) : ye(e) {
-            uint_t np = sizeof...(Args);
-            uint_t nm = n_elements(ye);
+        struct pack_tag {};
 
-            cache.resize(np,nm);
+    private :
+        void update_matrix_() {
+            uint_t np = cache.dims[0];
+            uint_t nm = cache.dims[1];
+
             beta.resize(np);
             alpha.resize(np,np);
-            impl::linfit_make_cache_(cache, ye, 0, std::forward<Args>(args)...);
 
             // Solving 'y +/- e = sum over i of a[i]*x[i]' to get all a[i]'s
-            for (uint_t i = 0; i < np; ++i)
-            for (uint_t j = 0; j < np; ++j) {
+            for (uint_t i : range(np))
+            for (uint_t j : range(np)) {
                 if (i <= j) {
                     alpha.safe(i,j) = 0.0;
                     // alpha(i,j) = sum over all points of x[i]*x[j]/e^2
-                    for (uint_t m = 0; m < nm; ++m) {
+                    for (uint_t m : range(nm)) {
                         alpha.safe(i,j) += cache.safe(i,m)*cache.safe(j,m);
                     }
                 } else {
                     alpha.safe(i,j) = alpha.safe(j,i);
                 }
             }
+        }
 
-            if (!matrix::inplace_invert_symmetric(alpha)) {
+        void update_matrix_(uint_t i) {
+            uint_t np = cache.dims[0];
+            uint_t nm = cache.dims[1];
+
+            for (uint_t j : range(np)) {
+                alpha.safe(i,j) = 0.0;
+                for (uint_t m : range(nm)) {
+                    alpha.safe(i,j) += cache.safe(i,m)*cache.safe(j,m);
+                }
+                alpha.safe(j,i) = alpha.safe(i,j);
+            }
+        }
+
+        void invert_matrix_() {
+            uint_t np = cache.dims[0];
+            uint_t nm = cache.dims[1];
+
+            if (!matrix::invert_symmetric(alpha, fr.cov)) {
                 fr.success = false;
                 fr.chi2 = dnan;
                 fr.params = replicate(dnan, np);
                 fr.errors = replicate(dnan, np);
             } else {
                 fr.success = true;
-                fr.errors = sqrt(matrix::diagonal(alpha));
+                fr.errors = sqrt(matrix::diagonal(fr.cov));
             }
 
-            matrix::symmetrize(alpha);
-            fr.cov = alpha;
+            matrix::symmetrize(fr.cov);
+        }
+
+        template<typename TypeX>
+        void update_model_(uint_t i, const TypeX& x) {
+            uint_t np = cache.dims[0];
+            uint_t nm = cache.dims[1];
+
+            phypp_check(i < np, "index goes beyond available models (", i, " vs. ", np, ")");
+
+            for (uint_t j : range(nm)) {
+                cache.safe(i,j) = x.safe[j]/ye.safe[j];
+            }
+
+            update_matrix_(i);
+        }
+
+    public:
+        template<typename ... Args>
+        linfit_batch_t(const TypeE& e, Args&&... args) : ye(e) {
+            uint_t np = sizeof...(Args);
+            uint_t nm = n_elements(ye);
+
+            cache.resize(np,nm);
+            impl::linfit_make_cache_(cache, ye, 0, std::forward<Args>(args)...);
+
+            update_matrix_();
+            invert_matrix_();
+        }
+
+        template<typename TypeX>
+        linfit_batch_t(pack_tag, const TypeE& e, const TypeX& x) : ye(e) {
+            uint_t np = x.dims[0];
+            uint_t nm = n_elements(ye);
+
+            cache.resize(np,nm);
+            for (uint_t i : range(np))
+            for (uint_t j : range(nm)) {
+                cache.safe(i,j) = x.safe[i*x.pitch(0) + j]/ye.safe[j];
+            }
+
+            update_matrix_();
+            invert_matrix_();
+        }
+
+        template<typename TypeX>
+        void update_model(uint_t i, const TypeX& x) {
+            update_model_(i, x);
+            invert_matrix_();
+        }
+
+        void update_models() {
+            invert_matrix_();
+        }
+
+        template<typename TypeX, typename ... Args>
+        void update_models(uint_t i, const TypeX& x, const Args& ... args) {
+            update_model_(i, x);
+            update_models(args...);
         }
 
         template<typename TypeY>
@@ -209,20 +283,20 @@ namespace phypp {
             uint_t nm = cache.dims[1];
 
             auto tmp = flatten(y/ye);
-            for (uint_t i = 0; i < np; ++i) {
+            for (uint_t i : range(np)) {
                 beta.safe[i] = 0.0;
                 // beta[i] = sum over all points of x[i]*y/e^2
-                for (uint_t m = 0; m < nm; ++m) {
+                for (uint_t m : range(nm)) {
                     beta.safe[i] += cache.safe(i,m)*tmp.safe[m];
                 }
             }
 
-            fr.params = matrix::product(alpha, beta);
+            fr.params = matrix::product(fr.cov, beta);
 
             vec1d model(nm);
-            for (uint_t m = 0; m < nm; ++m) {
+            for (uint_t m : range(nm)) {
                 model.safe[m] = 0.0;
-                for (uint_t i = 0; i < np; ++i) {
+                for (uint_t i : range(np)) {
                     model.safe[m] += fr.params.safe[i]*cache.safe(i,m);
                 }
             }
@@ -234,6 +308,12 @@ namespace phypp {
     template<typename TypeE, typename ... Args>
     linfit_batch_t<TypeE> linfit_batch(const TypeE& e, Args&&... args) {
         return linfit_batch_t<TypeE>(e, std::forward<Args>(args)...);
+    }
+
+    template<typename TypeE, typename ... Args>
+    linfit_batch_t<TypeE> linfit_pack_batch(const TypeE& e, Args&&... args) {
+        return linfit_batch_t<TypeE>(typename linfit_batch_t<TypeE>::pack_tag{},
+            e, std::forward<Args>(args)...);
     }
 }
 
