5e8170ea442e074628c3a825fbfc52474020a756
cschreib <cschreib@orange.fr>
2017-03-15 18:22:02 +0100

    Made FFTW usage thread safe and fixed ifft modifying input

--- include/phypp/math/fourier.hpp
+++ include/phypp/math/fourier.hpp
@@ -5,18 +5,30 @@
 #include <fftw3.h>
 #endif
 #include "phypp/core/vec.hpp"
+#include "phypp/utility/thread.hpp"
 #include "phypp/math/complex.hpp"
 
 namespace phypp {
+    namespace impl {
+        std::mutex& fftw_planner_mutex() {
+            static std::mutex m;
+            return m;
+        }
+    }
+
     #ifndef NO_FFTW
     // Compute the Fast Fourier Transform (FFT) of the provided 2d array
     inline vec2cd fft(const vec2d& v) {
         vec2cd r(v.dims);
 
         fftw_plan p;
-        p = fftw_plan_dft_r2c_2d(v.dims[0], v.dims[1],
-            const_cast<double*>(v.data.data()),
-            reinterpret_cast<fftw_complex*>(r.data.data()), FFTW_ESTIMATE);
+        {
+            std::lock_guard<std::mutex> lock(impl::fftw_planner_mutex());
+            p = fftw_plan_dft_r2c_2d(v.dims[0], v.dims[1],
+                const_cast<double*>(v.data.data()),
+                reinterpret_cast<fftw_complex*>(r.data.data()), FFTW_ESTIMATE);
+        }
+
         fftw_execute(p);
         fftw_destroy_plan(p);
 
@@ -24,13 +36,18 @@ namespace phypp {
     }
 
     // Compute the Fast Fourier Transform (FFT) of the provided 2d array
-    inline vec2d ifft(const vec2cd& v) {
+    // NB: the FFTW routine does not preserve the data in input, so the
+    // input array has to be copied
+    inline vec2d ifft(vec2cd v) {
         vec2d r(v.dims);
 
         fftw_plan p;
-        p = fftw_plan_dft_c2r_2d(v.dims[0], v.dims[1],
-            const_cast<fftw_complex*>(reinterpret_cast<const fftw_complex*>(v.data.data())),
-            r.data.data(), FFTW_ESTIMATE);
+        {
+            std::lock_guard<std::mutex> lock(impl::fftw_planner_mutex());
+            p = fftw_plan_dft_c2r_2d(v.dims[0], v.dims[1],
+                reinterpret_cast<fftw_complex*>(v.data.data()),
+                r.data.data(), FFTW_ESTIMATE);
+        }
         fftw_execute(p);
         fftw_destroy_plan(p);
 
