30d46b02f3f289cdf0a307a441437574377e41bd
cschreib <cschreib@orange.fr>
2017-05-09 20:52:34 +0200

    Added flat_id() overload with dims argument

--- include/phypp/utility/generic.hpp
+++ include/phypp/utility/generic.hpp
@@ -108,18 +108,6 @@ namespace phypp {
     }
 
     // Get multi-dim IDs from a flat ID
-    template<std::size_t D, typename T>
-    vec1u mult_ids(const vec<D,T>& v, uint_t i) {
-        vec1u r(D);
-
-        for (uint_t j : range(D)) {
-            r.safe[D-1-j] = i % v.dims[D-1-j];
-            i /= v.dims[D-1-j];
-        }
-
-        return r;
-    }
-
     template<std::size_t D>
     vec1u mult_ids(const std::array<uint_t,D>& dims, uint_t i) {
         vec1u r(D);
@@ -127,18 +115,6 @@ namespace phypp {
         for (uint_t j : range(D)) {
             r.safe[D-1-j] = i % dims[D-1-j];
             i /= dims[D-1-j];
-        }
-
-        return r;
-    }
-
-    template<std::size_t D, typename T>
-    vec2u mult_ids(const vec<D,T>& v, vec1u i) {
-        vec2u r(D,i.size());
-
-        for (uint_t j : range(D)) {
-            r.safe(D-1-j,_) = i % v.dims[D-1-j];
-            i /= v.dims[D-1-j];
         }
 
         return r;
@@ -156,24 +132,43 @@ namespace phypp {
         return r;
     }
 
+    template<std::size_t D, typename T>
+    vec1u mult_ids(const vec<D,T>& v, uint_t i) {
+        return mult_ids(v.dims, i);
+    }
+
+    template<std::size_t D, typename T>
+    vec2u mult_ids(const vec<D,T>& v, vec1u i) {
+        return mult_ids(v.dims, i);
+    }
+
     // Get flat ID from multi-dim IDs
     namespace impl {
-        template<std::size_t D, typename T>
-        uint_t flat_id_(const vec<D,T>& v, uint_t ret, meta::cte_t<D>) {
+        template<std::size_t D>
+        uint_t flat_id_(const std::array<uint_t,D>&, uint_t ret, meta::cte_t<D>) {
             return ret;
         }
 
-        template<std::size_t D, typename T, std::size_t I, typename U, typename ... Args>
-        uint_t flat_id_(const vec<D,T>& v, uint_t ret, meta::cte_t<I>, U i, Args&& ... args) {
-            return flat_id_(v, ret + v.pitch(I)*v.template to_idx<I>(i), meta::cte_t<I+1>{},
+        template<std::size_t D, std::size_t I, typename U, typename ... Args>
+        uint_t flat_id_(const std::array<uint_t,D>& dims, uint_t ret,
+            meta::cte_t<I>, U i, Args&& ... args) {
+
+            uint_t di = impl::vec_access::pitch(dims, I)*
+                impl::vec_access::template to_idx<I>(dims, i);
+            return flat_id_(dims, ret + di, meta::cte_t<I+1>{},
                 std::forward<Args>(args)...);
         }
     }
 
+    template<std::size_t D, typename ... Args>
+    uint_t flat_id(const std::array<uint_t,D>& dims, Args&& ... args) {
+        static_assert(sizeof...(Args) == D, "wrong number of IDs provided");
+        return impl::flat_id_(dims, 0, meta::cte_t<0>{}, std::forward<Args>(args)...);
+    }
+
     template<std::size_t D, typename T, typename ... Args>
     uint_t flat_id(const vec<D,T>& v, Args&& ... args) {
-        static_assert(sizeof...(Args) == D, "wrong number of IDs provided");
-        return impl::flat_id_(v, 0, meta::cte_t<0>{}, std::forward<Args>(args)...);
+        return flat_id(v.dims, std::forward<Args>(args)...);
     }
 
 
