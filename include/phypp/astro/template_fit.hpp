976047f2a604a0a4932e892a639f2bad02194371
cschreib <cschreib@orange.fr>
2017-05-22 15:07:54 +0200

    Simplified code to get chi2, I'm not sure what it was doing

--- include/phypp/astro/template_fit.hpp
+++ include/phypp/astro/template_fit.hpp
@@ -289,11 +289,16 @@ namespace astro {
             }
 
             res.amp = tmp1/tmp2;
+
+            res.chi2.resize(nsed);
             if (params.renorm) {
-                tmp1 *= res.amp;
-                res.chi2 = total(weight*flux*flux) - tmp1;
+                for (uint_t i = 0; i < nsed; ++i) {
+                    res.chi2.safe[i] = total(weight*sqr(flux - res.amp[i]*res.flux(i,_)));
+                }
             } else {
-                res.chi2 = total(weight*flux*flux) - 2.0*tmp1 + tmp2;
+                for (uint_t i = 0; i < nsed; ++i) {
+                    res.chi2.safe[i] = total(weight*sqr(flux - res.flux(i,_)));
+                }
             }
 
             // Find the best chi2 among all the SEDs
@@ -312,12 +317,16 @@ namespace astro {
                 }
 
                 auto amp = tmp1/tmp2;
-                vec<1,ttype> chi2;
+
+                vec<1,ttype> chi2(nsed);
                 if (params.renorm) {
-                    tmp1 *= amp;
-                    chi2 = total(weight*fsim*fsim) - tmp1;
+                    for (uint_t t = 0; t < nsed; ++t) {
+                        chi2.safe[t] = total(weight*sqr(fsim - amp[t]*res.flux(t,_)));
+                    }
                 } else {
-                    chi2 = total(weight*fsim*fsim) - 2.0*tmp1 + tmp2;
+                    for (uint_t t = 0; t < nsed; ++t) {
+                        chi2.safe[t] = total(weight*sqr(fsim - res.flux(t,_)));
+                    }
                 }
 
                 auto ised = min_id(chi2);
