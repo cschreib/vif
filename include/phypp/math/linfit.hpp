bb308d718833a987698aa350622b9f338f4da163
cschreib <cschreib@orange.fr>
2018-02-03 22:01:14 +0100

    Added missing include

--- include/phypp/math/linfit.hpp
+++ include/phypp/math/linfit.hpp
@@ -4,6 +4,7 @@
 #include "phypp/core/vec.hpp"
 #include "phypp/core/error.hpp"
 #include "phypp/core/range.hpp"
+#include "phypp/utility/idl.hpp"
 #include "phypp/math/base.hpp"
 #include "phypp/math/matrix.hpp"
 
