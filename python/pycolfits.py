e15d2f72d7d94dbaea85f963a10da2ba612334e0
Corentin Schreiber <github@corentin.net>
2021-09-23 08:30:10 +0100

    Apply suggestions from code review

--- python/pycolfits.py
+++ python/pycolfits.py
@@ -14,14 +14,8 @@ def readfrom(filename, lower_case=False, upper_case=False, **kwargs):
     data = hdulist[1].data[0]
     colnames = hdulist[1].data.dtype.names
     
-    ## check the length of columns to avoid any empty ones
-    checkcols = []
-    for cname in colnames:
-        try:
-            x = len(data[cname])
-            checkcols.append(cname)
-        except:
-            pass
+    # Check the length of columns to avoid any empty ones
+    checkcols = [x.name for x in hdulist[1].columns if re.search(r'[(,]0[),]', x.dim) is None]
 
     # Create columns one by one
     tbl = dict()
